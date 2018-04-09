8682908da182ddd62e0ab1babf6d7220dc1a3c58
Eric Boutueil <e.boutueil@gmail.com>
2018-04-09 23:53:24 +0200

    fix flow to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409204157) do
+ActiveRecord::Schema.define(version: 20180409215114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 20180409204157) do
   create_table "types", force: :cascade do |t|
     t.string "code"
     t.string "label"
-    t.string "flow"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "flow"
   end
 
   create_table "users", force: :cascade do |t|
