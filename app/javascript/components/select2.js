837e0f5afc5a3ba621f4d1313b3182fcf69772c9
Eric Boutueil <e.boutueil@gmail.com>
2018-04-10 10:39:26 +0200

    refacto select2 harbours + years OKKKKK

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -4,63 +4,93 @@ import 'select2';
 
 // HARBOURS
 
-// select2 field selections (1a)
+// (1a) selections for select2 fields
 $('#select2_harbours').select2({
     placeholder: "Ecrivez ou sélectionnez pour filtrer",
     allowClear: true
 });
-// requiring CSS (1b)
-import 'select2/dist/css/select2.css';
 
-// "submit" event listener (2)
-$('#select2_harbours').on("change", (event) => {
-  buildDataHash();
+$('#select2_years').select2({
+    placeholder: "Ecrivez ou sélectionnez pour filtrer",
+    allowClear: true
 });
 
-// built data in hash (3)
-function buildDataHash() {
-  let values = [];
+// (1b) requiring CSS
+import 'select2/dist/css/select2.css';
+
+// (2) "submit" event listeners
+$('#select2_harbours').on("change", (event) => {
+  buildData();
+});
+$('#select2_years').on("change", (event) => {
+  buildData();
+});
+
+// (3) built data in hash
+function buildData() {
+  var harbours = [];
   $('#select2_harbours').find("option:selected").each(function(i, selected){
-    values[i] = $(selected).text();
+    harbours[i] = $(selected).text();
   });
-  console.log("harbours after selected = " + values);
+  console.log("harbours selection(s) = " + harbours);
+
+  var years = [];
+  $('#select2_years').find("option:selected").each(function(i, selected){
+    years[i] = $(selected).text();
+  });
+  console.log("years selection(s) = " + years);
+
+  var values = {harbours, years};
+
   // calling ajax -> (4)
   callAjax(values);
 }
 
-// call ajax get (4)
+// (4) call ajax get
 function callAjax(values) {
   $.get({
     url: '/harbours',
     dataType: "script",
-    data: {name: values}
+    data: {name: values.harbours, year: values.years}
   });
 }
 
 
-// YEARS
+// // Functional
+// // HARBOURS
 
-$('#select2_years').select2({ // TBU for each filter
-    placeholder: "Ecrivez ou sélectionnez pour filtrer",
-    allowClear: true
-});
-import 'select2/dist/css/select2.css';
+// // (1a) select2 field selections
+// $('#select2_harbours').select2({ // TBU for each filter
+//     placeholder: "Ecrivez ou sélectionnez pour filtrer",
+//     allowClear: true
+// });
+// // (1b) requiring CSS
+// import 'select2/dist/css/select2.css';
 
-// event listener for each new selected year
-$('#select2_years').on("change", (event) => { // TBU for each filter
-// take ALL the (un)selected year
-  let values = [];
-  $(event.currentTarget).find("option:selected").each(function(i, selected){
-    values[i] = $(selected).text();
-  });
-  console.log("years after selected = " + values);
-  // call ajax get
-  $.get({
-    url: '/harbours',
-    dataType: "script",
-    data: {year: values} // TBU for each filter
-  });
-});
+// // (2) "submit" event listener
+// $('#select2_harbours').on("change", (event) => { // TBU for each filter
+//   buildDataHash();
+// });
+
+// // (3) built data in hash
+// function buildDataHash() {
+//   let values = [];
+//   $('#select2_harbours').find("option:selected").each(function(i, selected){
+//     values[i] = $(selected).text();
+//   });
+//   console.log("harbours after selected = " + values);
+//   // calling ajax -> (4)
+//   callAjax(values);
+// }
+
+// // (4) call ajax get
+// function callAjax(values) {
+//   $.get({
+//     url: '/harbours',
+//     dataType: "script",
+//     data: {name: values} // TBU for each filter
+//   });
+// }
 
 
 // console.log(event);
@@ -69,8 +99,8 @@ $('#select2_years').on("change", (event) => { // TBU for each filter
 // var harbourSelected = event.params.data.text;
 // console.log(harbourSelected); // -> bayonne  -> OK
 
+// Notes:
 // (1a): $ = document.querySelectorAll(...) for jquery plugin, and call select2 on it
 // (1b): == importing CSS! Path is relative to ./node_modules
 //       CSS included in JS (not in asset pipeline) and available in / compiled by Webpack
-
-// (3): Note: $(event.currentTarget ).find... -> event.currentTarget == '#select2_harbours'
+// (3):  $(event.currentTarget ).find... -> event.currentTarget == '#select2_harbours'
