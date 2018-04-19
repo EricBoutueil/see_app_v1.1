6c8d78a9ffe7be088e4f445a6f6675c4cba8111e
Eric Boutueil <e.boutueil@gmail.com>
2018-04-19 12:56:28 +0200

    hot final fix - v0.8

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -8,7 +8,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Traffic Port - See App V1.1"
+  config.site_title = "Port Traffic - See App V0.8"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
