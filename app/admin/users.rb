68f7fd7fd04ed0261bbde853a892f5a9587fb93a
Eric Boutueil <e.boutueil@gmail.com>
2018-05-04 20:50:18 +0200

    fix ok + some updates on users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 
   menu priority: 5, label: proc{ t('ar.model.users') }
 
-  actions :all
+  actions :all, except: [:new, :destroy]
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -17,14 +17,28 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
-  # permit_params :email, :first_name, :last_name, :company_name
+  permit_params :email, :first_name, :last_name, :phone_number, :company_name, :admin
+
+  form do |f|
+    inputs 'Details' do
+      # semantic_errors
+      input :email
+      input :first_name
+      input :last_name
+      input :phone_number
+      input :company_name
+      input :admin
+    end
+    actions
+  end
 
   index do
     selectable_column
     column :id
     column :email
     column :name
-    column :company_name
+    # column :company_name
+    # column :phone_number
     column :created_at
     column :last_sign_in_at
     column :sign_in_count
