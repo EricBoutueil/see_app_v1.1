da855c4d733578f9e725372dedfb20ddf0fbaee9
Eric Boutueil <e.boutueil@gmail.com>
2018-05-14 12:13:18 +0200

    actionmailer config for devise with mailjet

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :send_welcome_email
+
   # for active_admin
   def name
     "#{last_name} #{first_name}"
+  end
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
   end
 end
