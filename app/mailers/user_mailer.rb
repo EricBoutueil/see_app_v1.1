97db6ad18df53be2200b9e44d30be4667177af33
Colin Darie <colin@darie.eu>
2018-05-25 19:00:57 +0200

    Notification par mail à la fin des imports

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,15 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue dans le service Port Traffic proposé par See\'up')
     # This will render a view in `app/views/user_mailer`!
   end
+
+  def import_success(user)
+    @user = user
+    mail(to: @user.email, subject: "L'import est terminé")
+  end
+
+  def import_error(user, message)
+    @user = user
+    @message = message
+    mail(to: @user.email, subject: "L'import a échoué")
+  end
 end
