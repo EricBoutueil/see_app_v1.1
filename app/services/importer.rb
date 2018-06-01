933d687293ab621856f3959b36ad531150e60fcd
Eric Boutueil <e.boutueil@gmail.com>
2018-06-01 14:53:26 +0200

    updated rules for import harbours and types

--- app/services/importer.rb
+++ app/services/importer.rb
@@ -9,17 +9,19 @@ class Importer
     CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
       # updating if lat nil or creating harbours
       name = row[:name]&.downcase&.strip
+      address = row[:address]&.downcase&.strip
       code = row[:code]&.downcase&.strip
       flow = row[:flow]&.downcase&.strip
+      label = row[:label]&.downcase&.strip
 
-      update_or_create_harbour(name, row)
+      update_or_create_harbour(name, address, row)
 
       update_outre_mer("pointe-à-pitre", 48, -6.95)
       update_outre_mer("fort-de-france", 47, -6.95)
       update_outre_mer("dégrad des cannes", 46, -6.95)
       update_outre_mer("port réunion", 45, -6.95)
 
-      update_or_create_type(code, flow, row)
+      update_or_create_type(code, flow, label, row)
 
       update_or_create_movement(name, code, flow, row)
     end
@@ -27,37 +29,50 @@ class Importer
 
   private
 
-  def update_or_create_harbour(name, row)
-    return if name.nil?
+  def update_or_create_harbour(name, address, row)
+    return if name.nil? # if only updating types
+
+    return if address.nil? # if only updating movements
 
     scope = Harbour.where(name: name)
 
-    if scope.where(latitude: nil).exists?
+    if scope.exists?
       scope.update(
           country: row[:country],
-          address: row[:address].downcase,
+          address: address,
         )
       return
     end
 
-    if scope.where.not(latitude: nil).exists?
-      # NOOP
-      return
-    end
+    # # cancelled to be able to udpate an address with CSV updload. Condition now on address not nil.
+    # only update a harbour if latitude is nil in DB (because of Geocoding burden)
+    # if scope.where(latitude: nil).exists?
+      # scope.update(
+      #     country: row[:country],
+      #     address: address,
+      #   )
+      # return
+    # end
+    # if scope.where.not(latitude: nil).exists?
+    #   # NOOP
+    #   return
+    # end
 
     Harbour.create!(
       name: name,
       country: row[:country],
-      address: row[:address].downcase,
+      address: address,
     )
   end
 
-  def update_or_create_type(code, flow, row)
-    return if code.nil?
+  def update_or_create_type(code, flow, label, row)
+    return if code.nil? # if only updating harbours
+
+    return if label.nil? # if only updating movements
 
     # update of type without flow column
     if flow.nil?
-      hande_type_without_flow(row)
+      handle_type_without_flow(row)
       return
     end
 
@@ -65,7 +80,7 @@ class Importer
 
     if scope.exists?
       scope.update(
-        label: row[:label].downcase,
+        label: label,
         unit: row[:unit].downcase,
         description: row[:description].downcase
       )
@@ -76,10 +91,34 @@ class Importer
     Type.create!(
       code: code,
       flow: flow,
-      label: row[:label].downcase,
+      label: label,
       unit: row[:unit].downcase,
       description: row[:description].downcase
     )
+  end
+
+  def handle_type_without_flow(row)
+    scope = Type.where(code: code)
+
+    if scope.exists?
+      scope.update(
+        label: label,
+        unit: row[:unit].downcase,
+        description: row[:description]&.downcase
+      )
+
+      return
+    end
+
+    %w[imp exp].each do |flow|
+      Type.create!(
+        code: code,
+        flow: flow,
+        label: label,
+        unit: row[:unit].downcase,
+        description: row[:description]&.downcase
+      )
+    end
   end
 
   def update_or_create_movement(name, code, flow, row)
@@ -115,27 +154,4 @@ class Importer
     Harbour.where("LOWER(name) = ?", name).update(latitude: lat, longitude: lng)
   end
 
-  def hande_type_without_flow(row)
-    scope = Type.where(code: code)
-
-    if scope.exists?
-      scope.update(
-        label: row[:label]&.downcase,
-        unit: row[:unit].downcase,
-        description: row[:description]&.downcase
-      )
-
-      return
-    end
-
-    %w[imp exp].each do |flow|
-      Type.create!(
-        code: code,
-        flow: flow,
-        label: row[:label]&.downcase,
-        unit: row[:unit].downcase,
-        description: row[:description]&.downcase
-      )
-    end
-  end
 end
