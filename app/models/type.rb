bffa5b82039a2b48642ac6d58a5756be234fbaed
Colin Darie <colin@darie.eu>
2018-07-16 13:39:32 +0200

    Removed all classes variables from models
    This could lead to some problems in a multithread environment
    and could resolve the problem of empty harbours after an import.

--- app/models/type.rb
+++ app/models/type.rb
@@ -29,19 +29,19 @@ class Type < ApplicationRecord
 
   # for Select2 subfamilies1
   def self.filtered_subfamilies1(params) # before was: all_subfamilies1
-    @subfamilies1 = []
+    subfamilies1 = []
     if (params[:fam])
       x = params[:fam].first[0]
       Type.all.order(:code).each do |t|
-        unless @subfamilies1.include?({code: t.code, label: t.label})
+        unless subfamilies1.include?({code: t.code, label: t.label})
           if t.code.to_s[0] == x
             if t.code.to_s.length == 2
-              @subfamilies1 << {code: t.code, label: t.label}
+              subfamilies1 << {code: t.code, label: t.label}
             end
           end
         end
       end
-      return @subfamilies1
+      return subfamilies1
     else
       # # filter not needed for user because of automatic ajax call:
       # Type.all.each do |t|
@@ -53,48 +53,47 @@ class Type < ApplicationRecord
       #     end
       #   end
       # end
-      return @subfamilies1
+      return subfamilies1
     end
   end
 
   # for Select2 subfamilies2
   def self.filtered_subfamilies2(params)
-    @subfamilies2 = []
+    subfamilies2 = []
     if (params[:sub_one])
       x = params[:sub_one]
       Type.all.order(:code).each do |t|
-        unless @subfamilies2.include?({code: t.code, label: t.label})
+        unless subfamilies2.include?({code: t.code, label: t.label})
           if x.include?(t.code.to_s[0, 2])
             if t.code.to_s.length == 3
-              @subfamilies2 << {code: t.code, label: t.label}
+              subfamilies2 << {code: t.code, label: t.label}
             end
           end
         end
       end
-      return @subfamilies2
+      return subfamilies2
     else
-      return @subfamilies2
+      return subfamilies2
     end
   end
 
   # for Select2 subfamilies3
   def self.filtered_subfamilies3(params)
-    @subfamilies3 = []
+    subfamilies3 = []
     if (params[:sub_two])
       x = params[:sub_two]
       Type.all.order(:code).each do |t|
-        unless @subfamilies3.include?({code: t.code, label: t.label})
+        unless subfamilies3.include?({code: t.code, label: t.label})
           if x.include?(t.code.to_s[0, 3])
             if t.code.to_s.length == 4
-              @subfamilies3 << {code: t.code, label: t.label}
+              subfamilies3 << {code: t.code, label: t.label}
             end
           end
         end
       end
-      return @subfamilies3
+      return subfamilies3
     else
-      return @subfamilies3
+      return subfamilies3
     end
   end
-
 end
