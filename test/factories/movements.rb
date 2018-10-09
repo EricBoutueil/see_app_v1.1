1fb5f43d85b0955f6d2fd4bda44a976713ae2b0d
Colin Darie <colin@darie.eu>
2018-10-09 14:11:46 +0200

    Fixed factory bot deprecation for 5.0

--- test/factories/movements.rb
+++ test/factories/movements.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :movement do
     harbour
     type
-    year 2016
-    terminal ""
-    pol_pod ""
-    volume 1_000
+    year { 2016 }
+    terminal { "" }
+    pol_pod { "" }
+    volume { 1_000 }
   end
 end
--- test/factories/types.rb
+++ test/factories/types.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :type do
-    code "b1"
-    label "Some label"
-    description ""
-    flow "tot"
-    unit "tonnes"
+    code { "b1" }
+    label { "Some label" }
+    description { "" }
+    flow { "tot" }
+    unit { "tonnes" }
 
     trait :imp do
-      flow "imp"
+      flow { "imp" }
     end
 
     trait :exp do
-      flow "exp"
+      flow { "exp" }
     end
   end
 end
